a364106d8dd0df6d911c24c6173ffaac3c302146
Danilo <id4ni10@gmail.com>
2015-05-20 14:09:27 -0300

    Correção do path para estilos e scripts

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 var express = require('express'),
     app = express(),
     cons = require('consolidate'),
-    crypto = require('crypto'),
+	path = require('path'),
     MongoClient = require('mongodb').MongoClient;
 
 app.engine('html', cons.swig);
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
+app.use(express.static(path.join(__dirname, 'public')));
 
 MongoClient.connect('mongodb://localhost:27017/avaliacao', function(err, db) {
     if(err) throw err;
@@ -16,7 +17,6 @@ MongoClient.connect('mongodb://localhost:27017/avaliacao', function(err, db) {
 	});
     
     app.get('/pedidos', function(req, res){
-	
         return res.render('produtos', { "error" : " Página não encontrada!" });
     });
 
