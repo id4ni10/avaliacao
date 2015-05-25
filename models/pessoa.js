bcaae6e357521f375e0b862644d03041982074ff
Danilo <id4ni10@gmail.com>
2015-05-25 11:17:19 -0300

    Lixo

--- models/pessoa.js
+++
@@ -1,5 +0,0 @@
-var db = require('../lib/db_connect')(); 
-
-var Schema = require('mongoose').Schema; var pessoaSchema = Schema({ nome: String, email:String }); 
-
-var Pessoa = db.model('Pessoa', pessoaSchema); module.exports = Pessoa
